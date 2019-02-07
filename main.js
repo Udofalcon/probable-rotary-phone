468129ab89b253a1902969cb439d7d8126873fed
Udofalcon <Bear.Arms.Cavalry@gmail.com>
2019-02-06 21:10:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ var rows = 16;
 var columns = 16;
 var cells = [];
 var map = document.getElementById('map');
+var start = {
+    coordinates: [Math.floor(Math.random() * rows), Math.floor(Math.random() * columns)],
+    img: './cloth_14.png'
+};
+var goal = {
+    coordinates: [Math.floor(Math.random() * rows), Math.floor(Math.random() * columns)],
+    img: './cloth_5.png'
+};
 
 for (var x = 0; x < rows; x++) {
     var row = document.createElement('tr');
@@ -13,6 +21,7 @@ for (var x = 0; x < rows; x++) {
         var img = document.createElement('img');
 
         cells[x][y] = {
+            coordinates: [x, y],
             img: './clay.png'
         };
         img.setAttribute('src', cells[x][y].img);
@@ -23,3 +32,6 @@ for (var x = 0; x < rows; x++) {
 
     map.appendChild(row);
 }
+
+cells[start.coordinates[0]][start.coordinates[1]].img = start.img;
+cells[goal.coordinates[0]][goal.coordinates[1]].img = goal.img;
